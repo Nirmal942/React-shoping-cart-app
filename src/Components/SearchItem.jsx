150a71efad17f53eaba37aeca86b8f7379beb0ce
Nirmal <ns3237493@gmail.com>
2024-02-14 17:53:20 +0530

    React-shoping-cart-app

---
+++ src/Components/SearchItem.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'   // searchitem mai bhi useparams use kr rhe hai.
+import { items } from './Data';
+import Product from './Product';
+
+
+
+const SearchItem = ({cart, setCart}) => {
+    // console.log(useParams())
+    const { term } = useParams();
+
+    const [filterData, setFilterData] = useState([]);
+
+    useEffect(() => {
+
+        const filteredData = () => {
+            const data = items.filter((p)=>p.title.toLowerCase().includes(term.toLowerCase()));
+            // console.log(data)
+            setFilterData(data)
+          }
+      
+          filteredData();
+          
+        }, [term])
+        
+      
+    return (
+        <Product cart={cart} setCart={setCart} items={filterData} />       // React ks reusable component work here
+    )
+}
+
+export default SearchItem
